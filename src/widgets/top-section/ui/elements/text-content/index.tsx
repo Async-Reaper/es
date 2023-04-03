a17021e7c3be6b5100dfa70b7ede35d7b5017b28
ARSync <63187302+Async-Reaper@users.noreply.github.com>
2023-04-03 11:10:05 +0700

    -

--- src/widgets/top-section/ui/elements/text-content/index.tsx
+++ src/widgets/top-section/ui/elements/text-content/index.tsx
@@ -5,11 +5,8 @@ import cls from './styles.module.scss';
 const Component = () => (
    <div className={cls.content__text}>
       <Typography variant='h2' color='white-bg'>
-         Первая интеллектуальная платформа
+         Интеллектуальная платформа
          <span> для обмена педагогическим опытом</span>
-      </Typography>
-      <Typography variant='h3' color='gray-primary'>
-         от эксперта Светланы Курановой
       </Typography>
    </div>
 );
